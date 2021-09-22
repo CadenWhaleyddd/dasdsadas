0d6d6d9d7bb1cb2ed7f0ebbd0cb6b075f9cbc91e
CadenWhaleyddd <90707031+CadenWhaleyddd@users.noreply.github.com>
2021-09-22 12:53:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require('discord.js');
 const token = process.env.TOKEN
 const fs = require('fs');
 
@@ -14,8 +15,12 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
 const rest = new REST({ version: '9' }).setToken(token);
+client.login(token);
 
 (async () => {
 	try {
