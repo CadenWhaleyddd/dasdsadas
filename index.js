cfb757cf356ffe3bc74f42a007244987ccb0be32
CadenWhaleyddd <90707031+CadenWhaleyddd@users.noreply.github.com>
2021-09-23 10:39:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { Client, Intents } = require('discord.js');
-const token = process.env.TOKEN
+const Discord = require('discord.js');
 const fs = require('fs');
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// Place your client and guild ids here
-const clientId = '890223605865136189';
-const guildId = '876543210987654321';
+global.client = client
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-client.once('ready', () => {
-	console.log('Ready!');
+process.on('unhandledRejection', error => {
+    console.log(`UnhandledPromiseRejection : ${error}\n`)
 });
 
-const rest = new REST({ version: '9' }).setToken(token);
-client.login(token);
+client.on('ready', async () => {
+    
+    console.log(`\nLogged in : ${client.user.tag}\n`)
+    client.user.setActivity(`Slash!`, { type: "PLAYING" })
+        .then((presense) => console.log(`Set presense : ${presense.activities[0]}\n`))
+        .catch(console.error);
 
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.api.applications(client.user.id).guilds('749595288280498188').commands.post({ data: {
+            name: command.name,
+            description: command.description,
+            options: command.commandOptions
+        }})
+        if (command.global == true) {
+            client.api.applications(client.user.id).commands.post({ data: {
+                name: command.name,
+                description: command.description,
+                options: command.commandOptions
+            }})
+        }
+        client.commands.set(command.name, command);
+        console.log(`Command POST : ${command.name} from ${file} (${command.global ? "global" : "guild"})`)
+    }
+    console.log("")
+    
+    let cmdArrGlobal = await client.api.applications(client.user.id).commands.get()
+    let cmdArrGuild = await client.api.applications(client.user.id).guilds('749595288280498188').commands.get()
+    cmdArrGlobal.forEach(element => {
+        console.log("Global command loaded : " + element.name + " (" + element.id + ")" )
+    });
+    console.log("")
+    cmdArrGuild.forEach(element => {
+        console.log("Guild command loaded : " + element.name + " (" + element.id + ")")
+    });
+    console.log("")
+});
 
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+client.ws.on('INTERACTION_CREATE', async interaction => {
 
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
+    if (!client.commands.has(interaction.data.name)) return;
+
+    try {
+        client.commands.get(interaction.data.name).execute(interaction);
+    } catch (error) {
+        console.log(`Error from command ${interaction.data.name} : ${error.message}`);
+        console.log(`${error.stack}\n`)
+        client.api.interactions(interaction.id, interaction.token).callback.post({data: {
+			type: 4,
+			data: {
+					content: `Sorry, there was an error executing that command!`
+				}
+			}
+		})
+    }
+    
+})
+
+client.login(process.env.TOKEN);
